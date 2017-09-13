9ed1622dd1feb9fb4e4388028449074b3d523ee0
Don Bourne <dbourne@ca.ibm.com>
2017-09-13 14:14:28 -0400

    updates to enable TCK to run with a server with basic auth.  fixes #128
    Signed-off-by: Don Bourne <dbourne@ca.ibm.com>

--- tck/rest/src/main/java/org/eclipse/microprofile/metrics/test/MpMetricTest.java
+++ tck/rest/src/main/java/org/eclipse/microprofile/metrics/test/MpMetricTest.java
@@ -73,6 +73,7 @@ public class MpMetricTest {
     private static final String APPLICATION_JSON = "application/json";
     private static final String TEXT_PLAIN = "text/plain";
 
+    private static final String DEFAULT_PROTOCOL = "http";
     private static final String DEFAULT_HOST = "localhost";
     private static final int DEFAULT_PORT = 8080;
 
@@ -81,19 +82,30 @@ public class MpMetricTest {
 
     @BeforeClass
     static public void setup() throws MalformedURLException {
+        // set base URI and port number to use for all requests
         String serverUrl = System.getProperty("test.url");
-
+        String protocol = DEFAULT_PROTOCOL;
         String host = DEFAULT_HOST;
         int port = DEFAULT_PORT;
 
         if (serverUrl != null) {
             URL url = new URL(serverUrl);
+            protocol = url.getProtocol();
             host = url.getHost();
             port = (url.getPort() == -1) ? DEFAULT_PORT : url.getPort();
         }
 
-        RestAssured.baseURI = "http://" + host;
+        RestAssured.baseURI = protocol + "://" + host;
         RestAssured.port = port;
+
+        // set user name and password to use for basic authentication for all requests
+        String userName = System.getProperty("test.user");
+        String password = System.getProperty("test.pwd");
+
+        if (userName != null && password != null) {
+            RestAssured.authentication = RestAssured.basic(userName, password);
+            RestAssured.useRelaxedHTTPSValidation();
+        }
 
     }
 
