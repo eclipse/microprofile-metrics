e07e128d4f2777adb86cc02459d993ae6a15786f
Heiko W. Rupp <hrupp@redhat.com>
2018-07-17 13:59:39 +0200

    Double underscore are invalid.
    Signed-off-by: Heiko W. Rupp <hrupp@redhat.com>

---
+++ api/src/main/java/org/eclipse/microprofile/metrics/annotation/MetricAppName.java
@@ -0,0 +1,50 @@
+/*
+ * ********************************************************************
+ *  Copyright (c) 2018 Contributors to the Eclipse Foundation
+ *
+ *  See the NOTICES file(s) distributed with this work for additional
+ *  information regarding copyright ownership.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ * ********************************************************************
+ *
+ */
+package org.eclipse.microprofile.metrics.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Identifies an application name. This can be used to override
+ * the value obtained from <tt>javax.ws.rs.core.ApplicationPath#value</tt>
+ * and must be used if no <tt>ApplicationPath</tt> is present.
+ * @author Heiko W. Rupp <hrupp@redhat.com>
+ * @since 2.0
+ */
+@Documented
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MetricAppName {
+
+  /**
+   * The value that should be take as application name. This overrides an already
+   * existing value.
+   * @return The application name to use.
+   */
+  String value();
+}
