e6f55489eea1de941cf1e468093b064bc9be1446
chdavid <chdavid@ca.ibm.com>
2019-02-19 13:37:39 -0500

    Change MetricFilterTest to use WebArchive
    Signed-off-by: chdavid <chdavid@ca.ibm.com>

--- tck/api/src/main/java/org/eclipse/microprofile/metrics/tck/MetricFilterTest.java
+++ tck/api/src/main/java/org/eclipse/microprofile/metrics/tck/MetricFilterTest.java
@@ -30,7 +30,7 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,8 +39,8 @@ import org.junit.runner.RunWith;
 public class MetricFilterTest {
 
     @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class).addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class).addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
     private Metric metric = new Metric () { };
