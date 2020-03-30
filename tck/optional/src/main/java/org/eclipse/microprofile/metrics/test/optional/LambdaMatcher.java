6dfaa9e202d10e0534339611f5022f940e6d38d7
Jan Martiska <jmartisk@redhat.com>
2020-03-30 12:55:37 +0200

    Fix flaky MpMetricOptionalTest

---
+++ tck/optional/src/main/java/org/eclipse/microprofile/metrics/test/optional/LambdaMatcher.java
@@ -0,0 +1,49 @@
+/*
+ **********************************************************************
+ * Copyright (c) 2020 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICES file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ **********************************************************************/
+
+package org.eclipse.microprofile.metrics.test.optional;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+
+import java.util.function.Function;
+
+public class LambdaMatcher<T> extends BaseMatcher<T> {
+    private final Function<T, Boolean> matcher;
+    private final String description;
+
+    public LambdaMatcher(Function<T, Boolean> matcher,
+                         String description) {
+        this.matcher = matcher;
+        this.description = description;
+    }
+
+    @Override
+    public boolean matches(Object argument) {
+        return matcher.apply((T) argument);
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText(this.description);
+    }
+}
