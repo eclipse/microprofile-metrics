6578631887b15ef74f5b1dc29bc1ef9ee0739b1b
David Chan <chdavid@ca.ibm.com>
2020-05-05 14:48:05 -0400

    Handling of unmapped exceptions for REST metrics
    Signed-off-by: David Chan <chdavid@ca.ibm.com>

---
+++ tck/optional/src/main/java/org/eclipse/microprofile/metrics/test/optional/MpMetricsArithmeticExceptionMapper.java
@@ -0,0 +1,35 @@
+/*
+ **********************************************************************
+ * Copyright (c) 2020 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICES file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ **********************************************************************/
+package org.eclipse.microprofile.metrics.test.optional;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class MpMetricsArithmeticExceptionMapper implements ExceptionMapper<ArithmeticException>{
+
+    @Override
+    public Response toResponse(ArithmeticException ex) {
+        return Response.status(Response.Status.ACCEPTED).build();
+    }
+}
